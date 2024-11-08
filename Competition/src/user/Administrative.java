0c23741264f6a4f0ba332c67e8c9bf368fab48ba
justin <justindysarz01@gmail.com>
2024-11-08 08:25:27 -0700

    update java

--- Competition/src/user/Administrative.java
+++ Competition/src/user/Administrative.java
@@ -1,9 +1,13 @@
 package user;
 
-public class Administrative extends Person{
-	
-	public Administrative(String id, String userName) {
-		super(id, userName);
-	}
+public class Administrative extends Person {
+
+  public Administrative(String id, String userName) {
+    super(id, userName);
+  }
+
+  public final schedulingDashboard() {
+    //FIXME ADD GUI CONTROLER
+  }
 
 }
